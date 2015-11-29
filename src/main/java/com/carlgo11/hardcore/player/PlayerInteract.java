b68b7d5b718afdc27f16a6f38a04cb92ad1ddf2c
Carlgo11 <github@carlgo11.com>
2015-11-29 23:07:45 +0100

    Add more to PlayerInteract

--- src/main/java/com/carlgo11/hardcore/player/PlayerInteract.java
+++ src/main/java/com/carlgo11/hardcore/player/PlayerInteract.java
@@ -1,9 +1,15 @@
 package com.carlgo11.hardcore.player;
 
 import com.carlgo11.hardcore.Hardcore;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerInteract implements Listener {
 
@@ -14,8 +20,28 @@ public class PlayerInteract implements Listener {
         this.hc = parent;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onPlayerInteract(PlayerInteractEvent e)
+    {
+        Player player = e.getPlayer();
+        ItemStack item = e.getItem();
+        Material material = e.getMaterial();
+        Action action = e.getAction();
+        Block block = e.getClickedBlock();
+
+        if (material.equals(Material.COMPASS)) {
+            itemCompass(player, item, material, action, block);
+        } else if (material.equals(Material.WATCH)) {
+
+        }
+    }
+
+    private void itemCompass(Player player, ItemStack item, Material material, Action action, Block block)
+    {
+
+    }
+
+    private void itemWatch(Player player, ItemStack item, Material material, Action action, Block block)
     {
 
     }
