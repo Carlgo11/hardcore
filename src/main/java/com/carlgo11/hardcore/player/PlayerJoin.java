a1b516c937808de9483b0e9cc09993350f7ae699
Carlgo11 <github@carlgo11.com>
2015-12-06 02:42:25 +0100

    formatting

--- src/main/java/com/carlgo11/hardcore/player/PlayerJoin.java
+++ src/main/java/com/carlgo11/hardcore/player/PlayerJoin.java
@@ -24,19 +24,18 @@ public class PlayerJoin implements Listener {
         int state = hc.game().getGameState();
         if (state == 0) {
             hc.game().addPlayer(player);
-            int players = hc.getConfig().getInt("game.start-players")-hc.game().getPlayers().size();
+            int players = hc.getConfig().getInt("game.start-players") - hc.game().getPlayers().size();
             if (players <= 0) {
                 hc.game().startGame();
             } else {
                 player.setGameMode(GameMode.ADVENTURE);
-                hc.sendMessage(player, "Waiting for "+players+" more players...");
+                hc.sendMessage(player, "Waiting for " + players + " more players...");
             }
         } else if (state == 2) {
             hc.sendMessage(player, ChatColor.GOLD + "Game starting...");
         } else if (state == 1) {
             if (!player.hasPlayedBefore() && hc.getConfig().getBoolean("game.can-join-game")) {
                 hc.game().addPlayer(player);
-                player.setGameMode(GameMode.SURVIVAL);
                 e.setJoinMessage(ChatColor.GREEN + player.getName() + " joined the game!");
             } else {
                 player.setGameMode(GameMode.SPECTATOR);
