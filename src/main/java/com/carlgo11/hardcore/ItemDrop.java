76bf11f568c20672b5d61821c05196059273ace1
Carlgo11 <github@carlgo11.com>
2016-01-03 06:30:19 +0100

    Move ItemDrop item-list to a seperate file

--- src/main/java/com/carlgo11/hardcore/ItemDrop.java
+++ src/main/java/com/carlgo11/hardcore/ItemDrop.java
@@ -1,12 +1,13 @@
 package com.carlgo11.hardcore;
 
-import com.carlgo11.hardcore.ItemDrop.ItemObject;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class ItemDrop {
 
@@ -33,60 +34,31 @@ public class ItemDrop {
      */
     private ItemStack getItem(Player player)
     {
-        ArrayList<List> items = new ArrayList(hc.getConfig().getList("items"));
+       YamlConfiguration itemlist = YamlConfiguration.loadConfiguration(new File(hc.getDataFolder()+"/items.yml"));
+        ArrayList<List> items = new ArrayList(itemlist.getList("items"));
         int r = new Random().nextInt(items.size());
         ItemObject i = new ItemObject(items.get(r));
         ItemStack itemstack;
-        if (i.getData() == 0) {
-            itemstack = new ItemStack(i.getMaterial(), i.getSize());
+        if (i.data == 0) {
+            itemstack = new ItemStack(i.material, i.size);
         } else {
-            itemstack = new ItemStack(i.getMaterial(), i.getSize(), i.getData());
+            itemstack = new ItemStack(i.material, i.size, i.data);
         }
         return itemstack;
     }
 
-    public class ItemObject {
+    private class ItemObject {
+        private final Material material;
+        private final int size;
+        private short data;
 
-        private Material material;
-        private int size;
-        private short data=0;
-
-        public ItemObject(List list){
+        private ItemObject(List list){
             this.material=Material.getMaterial(String.valueOf(list.get(0)));
             this.size=(int) list.get(1);
             if(list.size() >= 3){
                 int n = (int) list.get(2);
             this.data= n > Short.MAX_VALUE ? Short.MAX_VALUE : n < Short.MIN_VALUE ? Short.MIN_VALUE : (short)n;
             }
-        }
-        public void setMaterial(Material material)
-        {
-            this.material = material;
-        }
-
-        public Material getMaterial()
-        {
-            return this.material;
-        }
-
-        public void setSize(int size)
-        {
-            this.size = size;
-        }
-
-        public int getSize()
-        {
-            return this.size;
-        }
-
-        public void setData(short data)
-        {
-            this.data = data;
-        }
-
-        public short getData()
-        {
-            return this.data;
         }
     }
 }
