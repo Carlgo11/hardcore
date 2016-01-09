eee40fedfff64f6ffabbe8c14a1d2c3020502943
Carlgo11 <github@carlgo11.com>
2016-01-09 04:48:20 +0100

    Shrink isWarmup() function

--- src/main/java/com/carlgo11/hardcore/Warmup.java
+++ src/main/java/com/carlgo11/hardcore/Warmup.java
@@ -23,10 +23,7 @@ public class Warmup implements Listener {
      */
     public boolean isWarmup()
     {
-        if (hc.game().getGameState() == 0) {
-            return true;
-        }
-        return false;
+        return hc.game().getGameState() == 0;
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
