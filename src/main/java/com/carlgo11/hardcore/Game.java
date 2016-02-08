ec2a3a79c3030f7c220b75cb2c0af0bc74dcb87e
Carlgo11 <github@carlgo11.com>
2016-02-08 11:53:03 +0100

    Add firework-spawning function

--- src/main/java/com/carlgo11/hardcore/Game.java
+++ src/main/java/com/carlgo11/hardcore/Game.java
@@ -1,39 +1,46 @@
 package com.carlgo11.hardcore;
 
 import java.util.ArrayList;
+import java.util.Random;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.scoreboard.Team;
 
 public class Game {
-    
+
     private final Hardcore hc;
     private int gamestate; // 0 = warmup, 1 = running, 2 starting, 3 ending
     private ArrayList<Player> players = new ArrayList<>();
     public int difficulty;
     private int loop;
     private final AlivePlayers getplayers;
-    
+
     public Game(Hardcore parent)
-    {
+      {
         this.hc = parent;
         this.getplayers = new AlivePlayers();
-    }
-    
+      }
+
     public AlivePlayers alivePlayers()
-    {
+      {
         return getplayers;
-    }
+      }
 
     /**
      * Set the game state to 1 = Running and start the {@link #loop loop}.
      */
     public void startGame()
-    {
+      {
         ArrayList<Player> plyrs = new ArrayList<>(Bukkit.getOnlinePlayers());
         alivePlayers().setPlayers(plyrs);
         for (Player p : plyrs) {
@@ -46,14 +53,14 @@ public class Game {
         setGameState(1);
         hc.getServer().getWorlds().get(0).setTime(hc.getConfig().getLong("game.start-time"));
         loop();
-    }
+      }
 
     /**
      * Change the difficulty and give {@link #alivePlayers() alive players} an
      * item every x minutes
      */
     private void loop()
-    {
+      {
         hc.broadcastMessage(ChatColor.GREEN + "Game started!");
         long time = 20 * 60 * hc.getConfig().getInt("difficulty.delay");
         loop = hc.getServer().getScheduler().scheduleSyncRepeatingTask(hc, () -> {
@@ -73,34 +80,34 @@ public class Game {
                 Bukkit.getScheduler().cancelTask(loop);
             }
         }, 20L, time);
-    }
+      }
 
     /**
      * Change the difficulty to the next level
      */
     private void nextDifficulty()
-    {
+      {
         difficulty = difficulty + (hc.getConfig().getInt("difficulty.addition"));
-    }
+      }
 
     /**
      * Stop the game, kick all players and stop the server.
      */
     public void stopGame()
-    {
+      {
         setGameState(3);
         Bukkit.getScheduler().cancelTask(loop);
         Bukkit.getScheduler().scheduleSyncDelayedTask(hc, new Runnable() {
             @Override
             public void run()
-            {
+              {
                 for (Player p : Bukkit.getOnlinePlayers()) {
                     p.kickPlayer("Game restarting...");
                 }
                 Bukkit.getServer().shutdown();
-            }
+              }
         }, (hc.getConfig().getInt("game.end-delay") * 20));
-    }
+      }
 
     /**
      * Returns current game difficulty
@@ -109,9 +116,9 @@ public class Game {
      * @see #difficulty
      */
     public int getDifficulty()
-    {
+      {
         return this.difficulty;
-    }
+      }
 
     /**
      * Get the current game state.
@@ -120,9 +127,9 @@ public class Game {
      * Ending
      */
     public int getGameState()
-    {
+      {
         return gamestate;
-    }
+      }
 
     /**
      * Set the current game state.
@@ -130,12 +137,12 @@ public class Game {
      * @param newstate The new game state
      */
     public void setGameState(int newstate)
-    {
+      {
         gamestate = newstate;
-    }
-    
+      }
+
     private void getGameEndMessage(ArrayList<Player> players, Player player)
-    {
+      {
         if (players.size() >= 1) {
             String aliveplayers = null;
             for (Player p : players) {
@@ -145,11 +152,40 @@ public class Game {
                     aliveplayers += ", " + p.getName();
                 }
             }
+            spawnFirework(players);
             hc.broadcastMessage(ChatColor.GREEN + "GAME ENDED! The Winners are " + aliveplayers + "!");
         } else {
+            spawnFirework(players);
             hc.broadcastMessage(ChatColor.GREEN + "GAME ENDED! The Winner is " + player.getName() + "!");
         }
-    }
+      }
+
+    /**
+     * Spawn a firework entity on the player(s)'s location
+     *
+     * @param players Players to spawn firework on.
+     */
+    private void spawnFirework(ArrayList<Player> players)
+      {
+        for (Player player : players) {
+            Firework fw = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
+            FireworkMeta fwm = fw.getFireworkMeta();
+
+            Random r = new Random();
+
+            Type[] types = Type.values();
+            int type = r.nextInt(types.length);
+
+            Color c1 = Color.fromRGB(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+            Color c2 = Color.fromRGB(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+
+            FireworkEffect effect = FireworkEffect.builder().flicker(r.nextBoolean()).withColor(c1).withFade(c2).with(types[type]).withTrail().build();
+            fwm.addEffect(effect);
+
+            fwm.setPower(0);
+            fw.setFireworkMeta(fwm);
+        }
+      }
 
     /**
      * A player might sometimes not have max health when the game starts. This
@@ -158,10 +194,10 @@ public class Game {
      * @param player Player to reset health to
      */
     private void resetPlayerHealth(Player player)
-    {
+      {
         player.setHealth(player.getMaxHealth()); //Player might have extra lifes due to custom server settings.
         player.setFoodLevel(20);
-    }
+      }
 
     public class AlivePlayers {
 
@@ -171,9 +207,9 @@ public class Game {
          * @return The alive players. If non are alive then null.
          */
         public ArrayList<Player> getPlayers()
-        {
+          {
             return players;
-        }
+          }
 
         /**
          * Add a new player to the game.
@@ -181,14 +217,14 @@ public class Game {
          * @param player New player
          */
         public void addPlayer(Player player)
-        {
+          {
             ArrayList<Player> plyrs = getPlayers();
             plyrs.add(player);
             setPlayers(plyrs);
             if (getGameState() == 1) {
                 player.setGameMode(GameMode.SURVIVAL);
             }
-        }
+          }
 
         /**
          * Remove a single player from the game.
@@ -196,7 +232,7 @@ public class Game {
          * @param player Player to remove from the game
          */
         public void removePlayer(Player player)
-        {
+          {
             if (players.contains(player)) {
                 player.setGameMode(GameMode.SPECTATOR);
                 ArrayList<Player> plyrs = getPlayers();
@@ -213,7 +249,7 @@ public class Game {
                     stopGame();
                 }
             }
-        }
+          }
 
         /**
          * Set alive players. Should not be used for adding or removing a single
@@ -222,9 +258,9 @@ public class Game {
          * @param aliveplayers Alive players
          */
         public void setPlayers(ArrayList<Player> aliveplayers)
-        {
+          {
             players = aliveplayers;
-        }
-        
+          }
+
     }
 }
