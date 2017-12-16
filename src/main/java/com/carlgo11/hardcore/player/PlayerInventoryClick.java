76fe6fc58e60622c84abc2c939140d18f4445dcc
Carlgo11 <Carlgo11@users.noreply.github.com>
2017-12-16 04:59:59 +0100

    Add wand menu

---
+++ src/main/java/com/carlgo11/hardcore/player/PlayerInventoryClick.java
@@ -0,0 +1,41 @@
+package com.carlgo11.hardcore.player;
+
+import com.carlgo11.hardcore.Hardcore;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerInventoryClick implements Listener {
+
+    private final Hardcore hc;
+
+    public PlayerInventoryClick(Hardcore parent) {
+        this.hc = parent;
+        this.setWandInventoryItems();
+    }
+
+    public static Inventory wandInventory = Bukkit.createInventory(null, 9, "Choose Wand Effect");
+
+    private void setWandInventoryItems() {
+        wandInventory.setItem(0, new ItemStack(Material.ARROW));
+        wandInventory.setItem(1, new ItemStack(Material.TNT));
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        ItemStack clicked = event.getCurrentItem();
+        Inventory inventory = event.getInventory();
+        int slot = event.getSlot();
+        if (inventory.getName().equals(wandInventory.getName())) {
+            event.setCancelled(true);
+            PlayerInteract.wandability = slot;
+            player.closeInventory();
+        }
+    }
+}
